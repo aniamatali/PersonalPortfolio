b332b7a6008d1bac08f06ef123201401b44999bd
alvin niamatali <aniamatali@icloud.com>
2018-01-21 18:38:30 -0800

    my website

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    console.log( "welcome!" );
+    $("#appendgit").click(function(){
+        alert("Sorry it's still under construction, but in the meantime why not visit my github repo! :)");
+        $("#github").show();
+    });
+});
